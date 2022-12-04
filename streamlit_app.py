00d25edb4a0c6230377e8bd9bbd838c7d0a10007
rchastagner <116068165+rchastagner@users.noreply.github.com>
2022-12-04 19:46:56 +0100

    Update streamlit_app.py
    Emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ streamlit.title('Title')
 
 streamlit.header('Header')
 
-streamlit.text('Text1')
-streamlit.text('Text2')
-streamlit.text('Text3')
+streamlit.header('🌹Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
